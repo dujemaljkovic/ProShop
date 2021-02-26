28dd5f2ca8f8c77f8930a22573712a8426d4798d
Duje Maljkovic <duje.maljkovic@gmail.com>
2021-02-26 20:39:01 +0100

    Product list reducer, action and Homescreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -10,16 +10,16 @@ export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const { data } = await axios.get('api/products');
+    const { data } = await axios.get('/api/products');
 
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
       payload:
-        error.respone && error.response.data.message
+        error.response && error.response.data.message
           ? error.response.data.message
-          : response.message,
+          : error.message,
     });
   }
 };
