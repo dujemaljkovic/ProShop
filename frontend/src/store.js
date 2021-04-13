14ac81bdf81d9165cac137fb1556695c02a97e41
Duje Maljkovic <duje.maljkovic@gmail.com>
2021-04-13 13:58:09 +0200

    Admin order list & mark as delivered

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -24,6 +24,7 @@ import {
   orderPayReducer,
   orderListMyReducer,
   orderListReducer,
+  orderDeliverReducer,
 } from './reducers/orderReducers';
 
 const reducer = combineReducers({
@@ -40,6 +41,7 @@ const reducer = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderDeliver: orderDeliverReducer,
   orderList: orderListReducer,
   orderListMy: orderListMyReducer,
   userList: userListReducer,
